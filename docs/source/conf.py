b8d5a4015b661ffd1b8c8418bd0969ed75b8d098
PLPOGBA <kangjinoo@knu.ac.kr>
2026-04-10 11:10:09 +0900

    Fix: rebuild sphinx with flask installed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,12 @@
 import sys
 import os
 
-# 프로젝트 루트 경로 추가 (app 모듈을 찾을 수 있게)
-sys.path.insert(0, os.path.abspath('../..'))
+# docs/source/ 기준으로 프로젝트 루트까지 올라가야 함
+sys.path.insert(0, os.path.abspath('../..'))  # ← web-service/ 루트를 가리켜야 함
 
-project = 'WEB-SERVICE'
-copyright = '2026, jinoo'
-author = 'jinoo'
-release = 'n'
-
-# ✅ 핵심 수정 부분
 extensions = [
-    'sphinx.ext.autodoc',   # automodule 디렉티브 활성화
-    'sphinx.ext.napoleon',  # Google Style docstring 파싱
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
 ]
 
 napoleon_google_docstring = True
-napoleon_numpy_docstring = False
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-language = 'python'
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
