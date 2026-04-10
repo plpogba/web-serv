90245de20d1df8633aa4f8deb928e42f7ec8e18b
PLPOGBA <kangjinoo@knu.ac.kr>
2026-04-10 11:05:12 +0900

    Fix: add autodoc, napoleon to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,27 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sys
+import os
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# 프로젝트 루트 경로 추가 (app 모듈을 찾을 수 있게)
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'WEB-SERVICE'
 copyright = '2026, jinoo'
 author = 'jinoo'
 release = 'n'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# ✅ 핵심 수정 부분
+extensions = [
+    'sphinx.ext.autodoc',   # automodule 디렉티브 활성화
+    'sphinx.ext.napoleon',  # Google Style docstring 파싱
+]
 
-extensions = []
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 language = 'python'
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
